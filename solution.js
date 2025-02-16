b3d05af3a04ba921bbf3f1ac5c55e5b87e7ed8f9
BugHunter-X <askzenai@gmail.com>
2025-02-16 22:16:40 +0100

    Add solution.js file

---
+++ solution.js
@@ -0,0 +1,32 @@
+```javascript
+const pipeline = [
+  {
+    $lookup: {
+      from: "collectionB",
+      localField: "_id",
+      foreignField: "foreignKey",
+      as: "relatedDocs",
+    },
+  },
+  {
+    $addFields: {
+      relatedDocs: {
+        $ifNull: ["$relatedDocs", []],
+      },
+    }
+  },
+  {
+    $unwind: "$relatedDocs",
+  },
+  {
+    $group: {
+      _id: "$_id",
+      relatedData: {
+        $push: "$relatedDocs.someField",
+      },
+    },
+  },
+];
+
+const result = await collectionA.aggregate(pipeline).toArray();
+```
